e2ab2957167bed5861cf7c77cdc0183ee27d49fd
сергей оразаев <cyperman@mail.ru>
2024-06-05 13:55:12 +0300

    complete first task

--- ForcesTask.cs
+++ ForcesTask.cs
@@ -1,20 +1,37 @@
-﻿namespace func_rocket;
+﻿using System;
+
+namespace func_rocket;
 
 public class ForcesTask
 {
-	/// <summary>
-	/// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
-	/// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
-	/// </summary>
-	public static RocketForce GetThrustForce(double forceValue) => r => Vector.Zero;
+    /// <summary>
+    /// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
+    /// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
+    /// </summary>
+    public static RocketForce GetThrustForce(double forceValue) => r =>
+    {
+        var direction = new Vector(Math.Cos(r.Direction), Math.Sin(r.Direction));
+        return direction * forceValue;
+    };
 
-	/// <summary>
-	/// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
-	/// </summary>
-	public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize) => r => Vector.Zero;
+    /// <summary>
+    /// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
+    /// </summary>
+    public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize) => r =>
+    {
+        return gravity(spaceSize, r.Location);
+    };
 
-	/// <summary>
-	/// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
-	/// </summary>
-	public static RocketForce Sum(params RocketForce[] forces) => forces[0];
+    /// <summary>
+    /// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
+    /// </summary>
+    public static RocketForce Sum(params RocketForce[] forces) => r =>
+    {
+        var totalForce = Vector.Zero;
+        foreach (var force in forces)
+        {
+            totalForce += force(r);
+        }
+        return totalForce;
+    };
 }
